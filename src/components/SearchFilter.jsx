63b058148353c749e030d1d15636b20009f00913
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-07 01:03:08 -0500

    implement function to filter countries by region

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -5,12 +5,12 @@ import { Context } from './Context';
 
 export default function SearchFilter(props) {
   const countrySearchValue = useRef();
+  const countryFilterValue = useRef();
 
   const { setCountries } = useContext(Context);
 
   async function handleSearch() {
     const searchValue = countrySearchValue.current.value;
-    console.log(searchValue);
 
     if (searchValue) {
       try {
@@ -22,10 +22,34 @@ export default function SearchFilter(props) {
       } catch (err) {
         console.error(err);
       }
-    } else {
+    } else if (searchValue === '') {
       try {
         const response = await fetch('https://restcountries.com/v2/all');
         const data = await response.json();
+        setCountries(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  async function handleFilter() {
+    const filterValue = countryFilterValue.current.value;
+
+    if (filterValue === 'All') {
+      try {
+        const response = await fetch('https://restcountries.com/v2/all');
+        const data = await response.json();
+        setCountries(data);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      try {
+        const res = await fetch(
+          `https://restcountries.com/v2/region/${filterValue}`
+        );
+        const data = await res.json();
         setCountries(data);
       } catch (err) {
         console.error(err);
@@ -47,10 +71,15 @@ export default function SearchFilter(props) {
           />
         </div>
 
-        <select id="region" name="region">
-          <option value="">Filter By Region</option>
+        <select
+          id="region"
+          name="region"
+          ref={countryFilterValue}
+          onChange={() => handleFilter()}
+        >
+          <option value="All">Filter By Region</option>
           <option value="Africa">Africa</option>
-          <option value="America">America</option>
+          <option value="Americas">Americas</option>
           <option value="Asia">Asia</option>
           <option value="Europe">Europe</option>
           <option value="Oceania">Oceania</option>
